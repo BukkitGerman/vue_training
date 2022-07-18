fea57c0285683a01bbf98128bd72f0449b0c37ba
Justin Preuß <justin.preuss@aks-service.de>
2022-07-18 15:53:58 +0200

    add ToDoForm component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>To-DO List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
@@ -12,20 +13,22 @@
 <script>
 import uniqueId from 'lodash.uniqueid';
 import ToDoItem from './components/ToDoItem';
+import ToDoForm from './components/ToDoForm';
 
 export default {
   name: 'App',
   components: {
-    ToDoItem
+    ToDoItem,
+    ToDoForm
+  },
+  methods: {
+    addToDo(toDoLabel) {
+      this.ToDoItems.push({id: uniqueId('todo-'), label: toDoLabel, done: false })
+    },
   },
   data() {
     return {
-      ToDoItems: [
-        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
-        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
-        { id: uniqueId('todo-'), label: 'Have fun', done: true },
-        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
-      ]
+      ToDoItems: []
     }
   }
 }
