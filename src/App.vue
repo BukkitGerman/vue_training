9fcfae8728bc1357149061ebbe694e57efe4c3a0
Justin Preuß <justin.preuss@aks-service.de>
2022-07-18 17:08:47 +0200

    add completed tasks

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <h1>To-DO List</h1>
     <to-do-form @todo-added="addToDo"></to-do-form>
-    <ul class="list-group w-50 row justify-content-center mx-auto mt-3">
+    <h2 id="list-summary">{{listSummary}}</h2>
+    <ul class="list-group w-50 row justify-content-center mx-auto mt-3" aria-labelledby="list-summary">
       <li class="list-group-item" v-for="item in ToDoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+        <to-do-item :label="item.label" :done="item.done" :id="item.id" @checkbox-changed="updateDoneStatus(item.id)"></to-do-item>
       </li>
     </ul>
   </div>
@@ -21,9 +22,19 @@ export default {
     ToDoItem,
     ToDoForm
   },
+  computed: {
+    listSummary() {
+      const numberFinishedItems = this.ToDoItems.filter(item =>item.done).length
+      return `${numberFinishedItems} out of ${this.ToDoItems.length} items completed`
+    }
+  },
   methods: {
     addToDo(toDoLabel) {
       this.ToDoItems.push({id: uniqueId('todo-'), label: toDoLabel, done: false })
+    },
+    updateDoneStatus(toDoId) {
+      const toDoToUpdate = this.ToDoItems.find(item => item.id === toDoId)
+      toDoToUpdate.done = !toDoToUpdate.done
     },
   },
   data() {
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox" :id="id" :checked="isDone"/>
+    <input class="form-check-input" type="checkbox" :id="id" :checked="isDone" @change="$emit('checkbox-changed')"/>
     <label class="form-check-label" :for="id">{{ label }}</label>
   </div>
 </template>
